bd7551bf7acd562ef24aac5f799176393bd0e4fc
mukesh22777 <shriramtecgno@gmail.com>
2020-03-31 11:46:46 -0400

    new up date 3/31/2020

---
+++ Automation_Real_Prectis_19.01_101/src/user_Story_101/varify_page_title.java
@@ -0,0 +1,56 @@
+package user_Story_101;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class varify_page_title {
+	  private FirefoxDriver driver;
+
+
+	@Test
+	  public void f() throws IOException {
+		
+		//varify the page title 
+		driver.manage().window().maximize();
+		driver.get("http://www.amazon.com");
+		System.out.println(driver.getTitle());
+		
+		
+		DateFormat date = new SimpleDateFormat("MM dd YYYY HH mm ss");
+		  Date d1 = new Date();
+		  String time = date.format(d1);
+		  System.out.println(time);
+		  
+		  File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		  
+		  
+		  FileUtils.copyFile(src, new File("C:\\Users\\MUKESH\\Desktop\\Image_Testing\\screenshot1.png"));
+		  
+		  File src1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		  FileUtils.copyFile(src1, new File("C:\\Users\\MUKESH\\Desktop\\Image_TestingImage_101 " + time + ".png"));
+	  }
+	  @BeforeClass
+	  public void beforeClass() {
+		  System.setProperty("webdriver.gecko.driver", "F:\\Selenium\\Lib\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			driver.get("https://www.amazon.com");
+	}
+
+	  @AfterClass
+	  public void afterClass() {
+		  driver.close();
+	  }
+	  
+
+	}
